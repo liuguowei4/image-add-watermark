8ea694f9d01388a00b5674db277e60afb2f90d07
liuguowei <guowei.liu@schillerchina.com>
2024-08-28 15:25:17 +0800

    增加可视化功能

--- index.py
+++ index.py
@@ -1,16 +1,27 @@
+# 应用程序部分
+import tkinter as tk
+from tkinter import ttk,messagebox
+# 操作水印部分
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-def add_watermark(input_folder, output_folder, watermark_text):
-    # 判断输出文件夹是否存在，不存创建一个
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+def add_watermark():
+    # 如果未填写默认为out文件夹，不存在时进行创建
+    output = input2.get("1.0","end").strip()
+    if output == "":
+        output=".\out"
+    if not os.path.exists(output):
+        os.makedirs(output)
     
+    input = input1.get("1.0","end").strip()
+    if input == "":
+        messagebox.showinfo("错误提示","输入文件夹的路径不能为空")
+        return
     # 遍历文件夹文件
-    for filename in os.listdir(input_folder):
+    for filename in os.listdir(input):
         # 如果是图片格式
         if filename.endswith(".jpg") or filename.endswith(".png"):
-            image_path = os.path.join(input_folder, filename)
+            image_path = os.path.join(input, filename)
             image = Image.open(image_path).convert("RGBA")
 
             # 创建相同大小的透明图层
@@ -23,22 +34,52 @@ def add_watermark(input_folder, output_folder, watermark_text):
             font = ImageFont.truetype("simhei.ttf", 50)
             
             # 获取文本宽高
-            text_bbox = d.textbbox((0, 0), watermark_text, font=font)
+            watermark = input3.get("1.0","end").strip()
+            if(watermark==""):
+                messagebox.showinfo("错误提示","水印文字不能为空")
+                return
+            text_bbox = d.textbbox((0, 0), watermark, font=font)
             text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
             
             # 设置水印的位置及颜色
             position = (image.width - text_width - 10, image.height - text_height - 10)
-            d.text(position, watermark_text, fill=(255, 255, 255, 128), font=font)
+            d.text(position, watermark, fill=(255, 255, 255, 128), font=font)
 
             # 合并图层
             watermarked = Image.alpha_composite(image, txt)
 
             # 转化为RGB模式，并保存到输出文件夹
-            output_image_path = os.path.join(output_folder, filename)
+            output_image_path = os.path.join(output, filename)
             watermarked.convert("RGB").save(output_image_path, "PNG")
 
             print(f"水印添加成功: {output_image_path}")
             
 
-# 批量添加水印
-add_watermark(".\input", ".\out", "Test水印")
+# 创建主窗口
+root = tk.Tk()
+root.title("图片批量水印")
+root.geometry("400x200")
+
+# 输入文件夹部分
+label = tk.Label(root, text="请输入要加水印图片的文件夹")
+label.pack()
+input1 = tk.Text(root, height=2)
+input1.pack()
+
+# 输出文件夹部分()
+label = tk.Label(root, text="请输入输出图片的文件夹(不填写默认out文件夹)")
+label.pack()
+input2 = tk.Text(root, height=2)
+input2.pack()
+
+# 输入水印部分
+label = tk.Label(root,text="请输入水印文字")
+label.pack()
+input3 = tk.Text(root,height=2)
+input3.pack()
+
+# 确然按钮
+button = ttk.Button(root, text="插入水印", command=add_watermark)
+button.pack(side=tk.BOTTOM)
+
+root.mainloop()
